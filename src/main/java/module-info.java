fe91e10509f9a53f963e12f4e0c2d873eba0aef0
Muhammad Wasif Ali Wasif <wasif35676@gmail.com>
2022-04-26 09:20:24 +0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module project.sportsequipmentmanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens project.sportsequipmentmanagementsystem to javafx.fxml;
+    exports project.sportsequipmentmanagementsystem;
+}
---
+++ src/main/java/project/sportsequipmentmanagementsystem/WelcomeScreenController.java
@@ -0,0 +1,12 @@
+package project.sportsequipmentmanagementsystem;
+
+import javafx.scene.control.Button;
+
+public class WelcomeScreenController {
+    //Defining buttons
+    public Button logInTeacher = new Button();
+    public Button logInStudent = new Button();
+    public Button logInPrincipal = new Button();
+
+
+}
