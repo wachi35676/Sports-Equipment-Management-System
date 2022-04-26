6d264b3f74a00a41068b144460557dd988de671b
Muhammad Wasif Ali Wasif <wasif35676@gmail.com>
2022-04-26 22:42:32 +0500

    Added First Principal Screen
    principal screen added with no working funcitonality yet

--- src/main/java/project/sportsequipmentmanagementsystem/WelcomeScreenController.java
+++ src/main/java/project/sportsequipmentmanagementsystem/WelcomeScreenController.java
@@ -1,6 +1,11 @@
 package project.sportsequipmentmanagementsystem;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WelcomeScreenController {
     //Defining buttons
@@ -8,5 +13,8 @@ public class WelcomeScreenController {
     public Button logInStudent = new Button();
     public Button logInPrincipal = new Button();
 
-
+    public void logInPrincipal (ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        new Main().changeSceneToPrincipalMenu(stage);
+    }
 }
